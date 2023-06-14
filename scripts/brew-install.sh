5ecebd2fc0f83cac6deca54238cac860aca453fb
Anthony Ciccarello <aciccarello@users.noreply.github.com>
2023-06-14 12:07:30 -0600

    fix brew-install syntax error

--- scripts/brew-install.sh
+++ scripts/brew-install.sh
@@ -1,6 +1,6 @@
 if ! command -v brew &> /dev/null
 then
-	command -v brew >/dev/null 2>&1 || {/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"}
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 	
 	echo "Brew installed. See 'Next steps' for further instructions"
 	echo "NOTE: Brewfile cannot be read till next steps are completed"
